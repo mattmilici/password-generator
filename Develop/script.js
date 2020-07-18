122e9d90bf5c40cd203924f6864d0f15da858103
Matthew Milici <milici.m915@gmail.com>
2020-07-17 19:33:45 -0700

    updated comments that explain how to use the app.

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,33 @@
-// The writePassword() Function will generate a random password based off of the users responses from the prompt & confirms below.
+// The writePassword() Function will generate a random password based off of the users responses from each prompt & confirms below.
 function writePassword() {
     // ---------------Start of Prompts and confirms that users are asked to answer----------
-    // Prompt that determines the users password length. It has to be between 8 and 128 characters. If it does not meet those requirements then the user will be asked to recreate a new password.
-    const pLength = prompt(
-        "How many characters do you want your password to be? Please enter a number between 8 and 128."
-    );
 
+    const pLength = prompt(
+        "How many characters do you want to include? Number must between 8 and 128."
+    );
     const pNumeric = confirm("Do you want to include numeric characters?");
     const pSpecial = confirm("Do you want to include special characters?");
     const pUpper = confirm("Do you want to include upppercase characters?");
     const pLower = confirm("Do you want to include lowercase characters?");
+
     // ---------------End of Prompts and confirms that users are asked to answer----------
 
-    // The letters variable is the string that will be used to genereate random characters using a charat(randomlength). The characters tied to each prompt will be added to letters only if the users confirms them.
+    // The letters variable is the string that random characters will be pulled from using a charat(randomlength). The characters tied to each prompt will be added to letters only if the users "confirms" them.
     var letters = "";
 
-    // checks are used to make sure the conditions are met. If these three conditions aren't greater than zero after password is generated then a new password will be generated. This while loop will run until all three conditions are greater than 0.
+    // Check variable (1 for each prompt/confrim) are used to make sure the conditions are met. If these three conditions aren't greater than zero after a password is generated then a new password will be generated until all 4 checks are greater than zero. Great than zero means that there is atleast one character in the password that meets the requirements
     let upperCheck = -1;
     let lowerCheck = -1;
     let numCheck = -1;
+    let specialCheck = -1;
 
     // if the user answers yes to an individual prompt/confirm then those characters will be added to the letters string. If they answer no, then the check will be set to 1000 so that is excluded in the while loop.
     pNumeric ? (letters = "0123456789") : ((letters = ""), (numCheck = 1000));
 
-    pSpecial ? (letters = letters + "%!@#$^$") : (letters = letters);
+    pSpecial
+        ?
+        (letters = letters + "%!@#$*&?") :
+        ((letters = letters), (specialCheck = 1000));
     pUpper
         ?
         (letters = letters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ") :
@@ -46,35 +50,41 @@ function writePassword() {
                 array.push(letter);
                 join = array.join("");
             }
-
             // checks to make sure the values are greater than 0
+            //if the user selected no for a condition, then the value for each variable below is already 1000 and should stay at 1000. If they selected yes, meaning they want to include said characters. Then we are calculating how many times the characters appear and returning the result.
             upperCheck =
                 upperCheck === 1000 ? 1000 : join.replace(/[^A-Z]/g, "").length;
+
             numCheck = numCheck === 1000 ? 1000 : join.replace(/[^0-9]/g, "").length;
+
             lowerCheck =
                 lowerCheck === 1000 ? 1000 : join.replace(/[^a-z]/g, "").length;
+
+            specialCheck =
+                specialCheck === 1000 ?
+                1000 :
+                join.replace(/[^%|!|@|#|$|*|&|?|]/g, "").length;
 
             console.log(upperCheck);
             console.log(lowerCheck);
             console.log(numCheck);
-            i = 0;
-            // the while loop will rerun as long as any of these three conditions are true
-        } while (numCheck < 1 || upperCheck < 1 || lowerCheck < 1);
+            console.log(specialCheck);
+
+            // the while loop will run as long as any of these three conditions are true. If they are true that means the password does not contain the requirements requested by the user.
+        } while (
+            numCheck < 1 ||
+            upperCheck < 1 ||
+            lowerCheck < 1 ||
+            specialCheck < 1
+        );
     } else {
         join =
             "Please enter your criteria again and be sure to not misstype anything!";
     }
 
     // -----------------Returns Paddword-----------------
-
     return (document.getElementById("password").innerHTML = join);
-}
+} //end of function
 
+//calls function writePassword when the user enters to "Generate Password" button
 document.getElementById("generate").addEventListener("click", writePassword);
-
-// -----------------Returns Paddword-----------------
-
-// let numCheckTest =
-// numCheck === 1000 ?
-// (numCheck = 1000) :
-// (numCheck = join.replace(/[^0-9]/g, "").length);
